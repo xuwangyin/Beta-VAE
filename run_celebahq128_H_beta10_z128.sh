00db54aa6888aa269b4e7f162404a9b56c342f44
Xuwang Yin <xuwangyin@gmail.com>
2020-03-31 17:13:59 -0400

    fix script beta value

--- run_celebahq128_H_beta10_z128.sh
+++ run_celebahq128_H_beta10_z128.sh
@@ -2,6 +2,6 @@
 
 python main.py --dataset celebahq128 --seed 1 --lr 1e-4 --beta1 0.9 --beta2 0.999 \
     --objective H --model H --batch_size 128 --z_dim 128 --max_iter 1.5e6 \
-    --beta 1 --viz_name celebahq128_H_beta10_z128 --dset_dir ./data \
+    --beta 10 --viz_name celebahq128_H_beta10_z128 --dset_dir ./data \
     --gather_step 1000 --display_step 5000
 
