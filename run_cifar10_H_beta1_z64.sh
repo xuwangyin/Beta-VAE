b0be136fa048f17d894790f6f17635123ad62459
Xuwang Yin <xuwangyin@gmail.com>
2020-04-13 15:23:57 -0400

    add cifar10 training script and checkpoint

---
+++ run_cifar10_H_beta1_z64.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+python main.py --dataset cifar10 --seed 1 --lr 1e-4 --beta1 0.9 --beta2 0.999 \
+    --objective H --model H --batch_size 128 --z_dim 64 --max_iter 1.5e6 \
+    --beta 1 --viz_name cifar10_H_beta1_z64 --dset_dir ./data --num_samples 10000
